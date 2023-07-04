395c3d76bf30de0704b67fcb29ed64e0fae95b0f
fjcloud <fj@msl.cloud>
2023-07-04 11:19:53 +0200

    Create rds.php

---
+++ src/rds.php
@@ -0,0 +1,43 @@
+<?php
+
+/********* CONFIG ********/
+$clusterEndpoint = "";
+$clusterPort = 3306;
+$clusterRegion = "us-east-1";
+$dbUsername = "";
+$dbDatabase = "";
+/*************************/
+
+// AWS-PHP-SDK installed via Composer
+require 'vendor/autoload.php';
+
+use Aws\Credentials\CredentialProvider;
+
+$provider = CredentialProvider::defaultProvider();
+$RdsAuthGenerator = new Aws\Rds\AuthTokenGenerator($provider);
+
+$token = $RdsAuthGenerator->createToken($clusterEndpoint . ":" . $clusterPort, $clusterRegion, $dbUsername);
+
+$mysqli = mysqli_init();
+
+mysqli_options($mysqli, MYSQLI_READ_DEFAULT_FILE, "./my.cnf");
+
+$mysqli->real_connect($clusterEndpoint, $dbUsername, $token, $dbDatabase, $clusterPort, NULL, MYSQLI_CLIENT_SSL);
+
+if ($mysqli->connect_errno) {
+    echo "Error: Failed to make a MySQL connection, here is why: <br />";
+    echo "Errno: " . $mysqli->connect_errno . "<br />";
+    echo "Error: " . $mysqli->connect_error . "<br />";
+    exit;
+}
+
+/***** Example code to perform a query and return all tables in the DB *****/
+$tableList = array();
+$res = mysqli_query($mysqli,"SHOW TABLES");
+while($cRow = mysqli_fetch_array($res))
+{
+    $tableList[] = $cRow[0];
+}
+echo '<pre>';
+print_r($tableList);
+echo '</pre>';
